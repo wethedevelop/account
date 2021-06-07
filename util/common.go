34cb3fcd980c739eae4eaf2e4f7125805bd7cfca
chengk <chengka666@163.com>
2021-06-07 16:35:25 +0800

    修复安全问题

--- util/common.go
+++ util/common.go
@@ -1,18 +1,23 @@
 package util
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"math/big"
 )
 
+// 随机函数封装
+func Rand(max int) int64 {
+	randNum, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	return randNum.Int64()
+}
+
 // RandStringRunes 返回随机字符串
-func RandStringRunes(n int) string {
+func RandStringRunes(n int64) string {
 	var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-	rand.Seed(time.Now().UnixNano())
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+		b[i] = letterRunes[Rand(len(letterRunes))]
 	}
 	return string(b)
 }
